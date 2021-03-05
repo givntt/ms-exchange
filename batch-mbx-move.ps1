c8ae7b998e627d57b2c1710c8a2348baa7259dba
givntt <75607693+givntt@users.noreply.github.com>
2021-03-05 16:29:41 +0100

    Update batch-mbx-move.ps1

--- batch-mbx-move.ps1
+++ batch-mbx-move.ps1
@@ -1,1 +1,5 @@
-import-csv c:\opt\Lotti_Migrazione\file.txt | %{New-MoveRequest -Identity $_.mail -TargetDatabase DBxx -BadItemLimit 5 -AcceptLargeDataLoss -suspendwhenreadytocomplete:$true -Bacthname Batchname}
+$batchName="batchname"
+$importFile=<path_to_import_file.txt>
+$targetDB="DB14-ITA"
+$badItemLimit=5
+import-csv $importFile | %{New-MoveRequest -Identity $_.mail -TargetDatabase $targetDB -BadItemLimit $badItemLimit -AcceptLargeDataLoss -suspendwhenreadytocomplete:$true -Bacthname $batchName}
