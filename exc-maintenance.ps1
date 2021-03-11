5a40329e726a1f11862696009f52b464cdb4a51b
givntt <75607693+givntt@users.noreply.github.com>
2021-03-11 21:32:31 +0100

    Update exc-maintenance.ps1

--- exc-maintenance.ps1
+++ exc-maintenance.ps1
@@ -1,10 +1,11 @@
+$exc="exc07"
 #Enter Maintenance Mode
-Set-ServerComponentState exc08 –Component HubTransport –State Draining –Requester Maintenance
-Redirect-Message –Server exc08 –Target exc07
-Set-ServerComponentState exc08 –Component ServerWideOffline –State InActive –Requester Maintenance
+Set-ServerComponentState $exc –Component HubTransport –State Draining –Requester Maintenance
+Redirect-Message –Server $exc –Target exc07
+Set-ServerComponentState $exc –Component ServerWideOffline –State InActive –Requester Maintenance
 #Exit Maintenance Mode
-Set-ServerComponentState exc08 -Component ServerWideOffline -State Active -Requester Maintenance
-Set-ServerComponentState exc08 -Component HubTransport -State Active -Requester Maintenance
+Set-ServerComponentState $exc -Component ServerWideOffline -State Active -Requester Maintenance
+Set-ServerComponentState $exc -Component HubTransport -State Active -Requester Maintenance
 #Get Component State
-Get-ServerComponentState exc08 | Select Component, State
+Get-ServerComponentState $exc | Select Component, State
 
